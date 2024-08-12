7c63f0ee2fe8b1a55249f6bf90b2176939d2cbc3
NOTEBOOKJOAO\joaom <joaomello0016@gmail.com>
2024-08-12 11:25:03 -0300

    Ajustes

---
+++ DesomaxBack/DesomaxBack/Migrations/20240812140234_AddingUserNameField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesomaxBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingUserNameField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+        }
+    }
+}
